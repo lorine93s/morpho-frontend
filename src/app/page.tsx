1113e7727d7f687a35471274d5d1ee4b93d932fe
Jian Lim <lorine93skilesvca@hotmail.com>
2025-03-17 21:33:21 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Page() {
           className='mt-[2vh] text-xl w-full rounded-[16px] py-2 px-6 bg-[#456DB5]'
         >
             Start Earning
+          
           </button>
       </Link>
     </div>
